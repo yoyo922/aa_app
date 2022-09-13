06851cfa92314442affdf9998b8b6f2497558423
peter395 <yong@exacttechnology.com>
2022-09-12 23:46:19 -0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="city-header">
+          <button>OTTAWA</button>
+          <button>MOSCOW</button>
+          <button>TOKYO</button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
