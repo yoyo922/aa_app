da9922b462204e458ade6e9db33960a6ba07b581
peter395 <yong@exacttechnology.com>
2022-09-13 00:28:32 -0400

    Fixed type script issue. added interface to component  to fix state. added onclick events

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,49 @@
 import "./App.css";
 import React from "react";
 
-class App extends React.Component {
-  render() {
+interface myState {
+  city: string
+}
+
+const cities = ["Ottawa", "Moscow", "Tokyo"];
+
+class App extends React.Component<{}, myState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      city: "Ottawa",
+    };
+  }
+
+  /**
+   * call back to handle onclick event to select cities
+   * will set component to be the user selected city
+   * @param e - button click event
+   */
+  setCity = (e: any) => {
+    this.setState({
+      city: e.target.name,
+    });
+  };
+
+  render() {  
     return (
       <div className="App">
         <div className="city-header">
-          <button>OTTAWA</button>
-          <button>MOSCOW</button>
-          <button>TOKYO</button>
+          {cities.map((city) => (
+            <button
+              key={city}
+              className={this.state.city === city ? "selected" : ""}
+              name={city}
+              onClick={this.setCity}
+            >
+              {city}
+            </button>
+          ))}
         </div>
       </div>
     );
   }
 }
 
-export default App
+export default App;
