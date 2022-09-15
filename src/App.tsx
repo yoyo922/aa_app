814621c94e80e03bf504aa87758916288c8f8ad0
peter395 <yong@exacttechnology.com>
2022-09-15 18:54:12 -0400

    added check for edge case where last element of array is a new day

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import WeatherDisplay from "./WeatherComponent/WeatherDisplay";
 interface myState {
   city: string;
   weatherData: any;
-  todayWeather: any
+  todayWeather: any;
 }
 
 const cities = ["Ottawa", "Moscow", "Tokyo"];
 //API key for openeweather API
 const api_key = "ceaabd1441aec5eb12417e86fc7f7b4f";
+const daysToCheck = 4;
 
 interface latLng {
   lat: string;
@@ -76,7 +77,7 @@ class App extends React.Component<{}, myState> {
         });
       });
 
-      axios
+    axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?lat=${
           cityLocations[city as keyof locations].lat
@@ -94,28 +95,30 @@ class App extends React.Component<{}, myState> {
   /**
    * Function to parse information needed from openweather API response
    * it will retrieve temperature, weather, icon and day as needed information
-   * for the child component 
+   * for the child component
    * @param weatherData - api response from openweather api
    * @returns - the needed information
    */
   getFourDaysWeather = (weatherData: Array<any>): Array<any> => {
-
     let fiveDaysWeather: any = [];
     let weatherTemp: number = 0;
     let weatherTempCount = 0;
     let weatherType: string = weatherData[0].weather[0].main;
     let weatherIcon: string = weatherData[0].weather[0].icon;
-    let timeString: string = weatherData[0].dt_txt.split(" ")[1]
-    let dayString: string = weatherData[0].dt_txt.split(" ")[0]
+    let timeString: string = weatherData[0].dt_txt.split(" ")[1];
+    let dayString: string = weatherData[0].dt_txt.split(" ")[0];
     let dayStamp: string = weatherData[0].dt;
-    
+
     weatherData.forEach((element, index) => {
-      if(dayString !== element.dt_txt.split(" ")[0] || index === weatherData.length - 1) {
+      if (
+        dayString !== element.dt_txt.split(" ")[0] &&
+        fiveDaysWeather.length < daysToCheck
+      ) {
         fiveDaysWeather.push({
           day: dayStamp,
           temp: Math.round(weatherTemp / weatherTempCount),
           weather: weatherType,
-          icon: weatherIcon
+          icon: weatherIcon,
         });
 
         weatherType = "";
@@ -123,21 +126,32 @@ class App extends React.Component<{}, myState> {
         weatherTemp = 0;
         weatherTempCount = 0;
       }
+      // check for edge case where last element of array is a new day
+      if (
+        index === weatherData.length - 1 &&
+        fiveDaysWeather.length < daysToCheck &&
+        weatherData[0].dt_txt.split(" ")[0] !== dayString
+      ) {
+        fiveDaysWeather.push({
+          day: element.dt,
+          temp: Math.round(element.main.temp),
+          weather: element.weather[0].main,
+          icon: element.weather[0].icon,
+        });
+      }
 
       dayString = element.dt_txt.split(" ")[0];
       timeString = element.dt_txt.split(" ")[1];
       dayStamp = element.dt;
 
-      if(timeString === '12:00:00') {
+      if (timeString === "12:00:00") {
         weatherType = element.weather[0].main;
         weatherIcon = element.weather[0].icon;
       }
 
       weatherTemp = weatherTemp + element.main.temp;
       weatherTempCount = weatherTempCount + 1;
-
     });
-    fiveDaysWeather.pop();
     return fiveDaysWeather;
   };
 
@@ -174,7 +188,11 @@ class App extends React.Component<{}, myState> {
             </button>
           ))}
         </div>
-        <WeatherDisplay city={city} fourDaysData={weatherData} todayData={todayWeather}></WeatherDisplay>
+        <WeatherDisplay
+          city={city}
+          fourDaysData={weatherData}
+          todayData={todayWeather}
+        ></WeatherDisplay>
       </div>
     );
   }
